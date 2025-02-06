e03c9572f4d995737d09b56bcfdf59585a2c16a7
Tani-2007 <saxenatanishq4567@gmail.com>
2025-02-06 20:23:54 +0530

    first

--- cricketProject/src/Error.jsx
+++ cricketProject/src/Error.jsx
@@ -9,7 +9,7 @@ const Error = () => {
         className="imageError"
         src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif"
         alt=""
-      />{" "}
+      />
       <button className="errorGoBack">
         <NavLink className="errGoBack" to={"/"}>
           GO BACK
--- cricketProject/src/News.jsx
+++ cricketProject/src/News.jsx
@@ -6,7 +6,7 @@ export function News() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch(
-      `https://api.worldnewsapi.com/search-news?text=${input}&language=en&earliest-publish-date=2025-02-05&api-key=573b95fdcd8b48cc85e7da5ed41d7b17`
+      `https://api.worldnewsapi.com/search-news?text=${input}&language=en&earliest-publish-date=2025-02-05&api-key=18914d2ce7c242b7ad31cb0c707421dc`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -51,7 +51,6 @@ export function News() {
             <div key={curNews.id} className="news-container">
               <p className="news-title">{curNews.title}</p>
               <img src={curNews.image} alt="news.image" />
-              <p>{curNews.text}</p>
               <a href={curNews.url} target="_blank">
                 Read More
               </a>
