0aab521161af418413091eb7e93c21fa888c1eb9
Tani-2007 <saxenatanishq4567@gmail.com>
2025-02-10 22:10:18 +0530

    first

--- cricketProject/src/Home.jsx
+++ cricketProject/src/Home.jsx
@@ -10,30 +10,41 @@ const Home = () => {
     setShowMenu(!showMenu);
   };
   return (
-    <header>
-      <div className="home-container">
-        <div className="home-grid navbar-grid">
-          <div className="home-logo">
-            <h1>Tanishq</h1>
-          </div>
-          <nav className={showMenu ? "menu-mobile" : "menu-web"}>
-            <ul>
-              <li>
-                <NavLink to={"/news"}>NEWS</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/cricket"}>Cricket</NavLink>
-              </li>
-            </ul>
-          </nav>
-          <div className="ham-menu">
-            <button onClick={handleButtonToggle}>
-              <ImMenu />
-            </button>
+    <div className="main-body">
+      <header>
+        <div className="home-container">
+          <div className="home-grid navbar-grid">
+            <div className="home-logo">
+              <h1>Tanishq</h1>
+            </div>
+            <nav className={showMenu ? "menu-mobile" : "menu-web"}>
+              <ul>
+                <li>
+                  <NavLink to={"/news"}>NEWS</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/cricket"}>Cricket</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/cricket"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/cricket"}>Contact</NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className="ham-menu">
+              <button onClick={handleButtonToggle}>
+                <ImMenu />
+              </button>
+            </div>
           </div>
         </div>
+      </header>
+      <div className="outside-cricket-home">
+        <div className="cricket-home"></div>
       </div>
-    </header>
+    </div>
   );
 };
 
